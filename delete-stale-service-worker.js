0850e03f363d987b63571f08a442a959fae7bd62
Gabor Dolla <gdolla@ximein.com>
2015-12-06 10:20:38 +0100

    almost works :)

--- delete-stale-service-worker.js
+++ delete-stale-service-worker.js
@@ -1,4 +1,13 @@
+
+// credit where credit's due
+// ideas/code from:
+// https://www.theguardian.com/service-worker.js
+// https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker
+// https://github.com/w3c-webmob/ServiceWorkersDemos
+
 var CACHE_NAME = 'gitt';
+var VERSIONKEY = 'version';
+var CHECKURL = '/update/';
 
 function doesRequestAcceptHtml(request) {
   return request.headers.get('Accept')
@@ -7,60 +16,102 @@ function doesRequestAcceptHtml(request) {
 };
 
 function cacheOrNetwork(event) {
-  event.respondWith(
-    caches.match(event.request)
+  return caches.match(event.request)
+  .then(function(response) {
+    console.log(event.request);
+    if (response) {
+      return response;
+    }
+
+    var fetchRequest = event.request.clone();
+    return fetch(fetchRequest)
     .then(function(response) {
-      if (response) {
-        console.log('from cache', event.request);
+      if(!response || response.status !== 200 || response.type !== 'basic') {
         return response;
       }
 
-      var fetchRequest = event.request.clone();
-      return fetch(fetchRequest)
-      .then(function(response) {
-        console.log('from network');
-        if(!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
+      var responseToCache = response.clone();
 
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
+      caches.open(CACHE_NAME)
+      .then(function(cache) {
+        cache.put(event.request, responseToCache);
       });
-    })
-  );
+      return response;
+    });
+  });
+};
+
+function getCurrentVersion() {
+  return caches.match(VERSIONKEY)
+  .then(function(version) {
+    if (version === undefined) {
+      return 'initial'; // FIXME find something better :)
+    }
+    return version.json();
+  });
+}
+
+function setCurrentVersion(version) {
+  return caches.open(VERSIONKEY)
+  .then(function(cache) {
+    var key = new Request(VERSIONKEY);
+    var value = new Response(version);
+    return cache.put(key, value);
+  });
+}
+
+function needToDelete(key, filenames) {
+  console.log(key, filenames);
+  return filenames && filenames.length ? filenames.some(function(filename) { return filename === key; }) : false;
+}
+
+function deleteFiles(filenames) {
+  return caches.keys()
+  .then(function(keys) {
+    return Promise.all(
+      keys.map(function(key) {
+        if (needToDelete(key, filenames)) {
+          console.log('deleting', key);
+          return caches.delete(key);
+        }
+      })
+    );
+  });
 };
 
 function validateCache() {
-  console.log('validate');
-  return Promise.resolve();
+  return new Promise(function(resolve) {
+    getCurrentVersion()
+    .then(function(version) {
+      fetch(CHECKURL + version + '.json' + '?'+Math.random())
+      .then(function(response) { return response.json() })
+      .then(function(json) {
+        if (version === json.version) {
+          return resolve();
+        }
+        return resolve(Promise.all([deleteFiles(json.modifiedFiles), setCurrentVersion(json.version)]));
+      });
+    });
+  });
 }
 
-this.addEventListener('install', function (event) {
-  console.log('install');
-});
-
-this.addEventListener('activate', function (event) {
-  console.log('activate');
-});
-
-this.addEventListener('fetch', function (event) {
+function handleEvent(event) {
   var request = event.request;
   var url = new URL(request.url);
   var isRootRequest = url.host === self.location.host;
-
   if (isRootRequest && doesRequestAcceptHtml(request)) {
     return validateCache()
     .then(function() {
-      cacheOrNetwork(event)
+      return cacheOrNetwork(event)
     });
   }
   else {
-    cacheOrNetwork(event);
+    return cacheOrNetwork(event);
   }
+}
+
+this.addEventListener('fetch', function (event) {
+  event.respondWith(
+    handleEvent(event)
+  );
 });
